d6a78b7ee268f39ec0a688ae049e1e377cc41e5e
kjian279 <kjian279@gmail.com>
2013-09-12 21:38:36 -0400

    Updated parser

--- src/springies/Parser.java
+++ src/springies/Parser.java
@@ -29,16 +29,29 @@ public class Parser {
 			
 			System.out.println("test");
 			createMasses(doc.getElementsByTagName("mass"));
-			
-			
-			NodeList muscles = doc.getElementsByTagName("muscle");
-			NodeList fixedMasses = doc.getElementsByTagName("fixed");
-			NodeList springs = doc.getElementsByTagName("spring");
+			createFixedMasses(doc.getElementsByTagName("fixed"));
+			createSprings(doc.getElementsByTagName("spring"));
+			createMuscles(doc.getElementsByTagName("muscle"));
 		} catch(Exception ex){
 			ex.printStackTrace();
 		}
 	}
+
+	public void createFixedMasses(NodeList masses){
+		for (int i=0; i<masses.getLength(); i++){
+			Node currMassObject = masses.item(i);
+			NamedNodeMap currMassAttr = currMassObject.getAttributes();
+			Node currID = currMassAttr.getNamedItem("id");
+			Node currMass = currMassAttr.getNamedItem("mass");
+			Node currX = currMassAttr.getNamedItem("x");
+			Node currY = currMassAttr.getNamedItem("y");
+			
+			Mass mass = new Mass(currID.getNodeValue(),1,JGColor.blue,10,5);//Double.parseDouble(currMass.getNodeValue()));
+			mass.setPos(Double.parseDouble(currX.getNodeValue()),Double.parseDouble(currY.getNodeValue()));
+		}
+	}
 	
+	//fix later
 	public void createMasses(NodeList masses){
 		for (int i=0; i<masses.getLength(); i++){
 			Node currMassObject = masses.item(i);
@@ -50,10 +63,32 @@ public class Parser {
 			Node currvX = currMassAttr.getNamedItem("vx");
 			Node currvY = currMassAttr.getNamedItem("vy");
 			
-			PhysicalObjectBouncyBall mass = new PhysicalObjectBouncyBall(currID.getNodeValue(),1,JGColor.blue,10,5);//Double.parseDouble(currMass.getNodeValue()));
+			Mass mass = new Mass(currID.getNodeValue(),1,JGColor.blue,10,5,Float.parseFloat(currvX.getNodeValue()),Float.parseFloat(currY.getNodeValue()));//Double.parseDouble(currMass.getNodeValue()));
 			mass.setPos(Double.parseDouble(currX.getNodeValue()),Double.parseDouble(currY.getNodeValue()));
 		}
 	}
+
 	
+	public void createSprings(NodeList springs){
+		for (int i=0; i<springs.getLength(); i++){
+			Node currSpring = springs.item(i);
+			NamedNodeMap springAttr = currSpring.getAttributes();
+			Node massA = springAttr.getNamedItem("a");
+			Node massB = springAttr.getNamedItem("b");
+			Node length = springAttr.getNamedItem("restLength");
+			Node k = springAttr.getNamedItem("constant");
+		}
+	}
 	
+	public void createMuscles(NodeList muscles){
+		for (int i=0; i<muscles.getLength(); i++){
+			Node currMuscle = muscles.item(i);
+			NamedNodeMap springAttr = currMuscle.getAttributes();
+			Node massA = springAttr.getNamedItem("a");
+			Node massB = springAttr.getNamedItem("b");
+			Node length = springAttr.getNamedItem("restLength");
+			Node k = springAttr.getNamedItem("constant");
+			Node amplitude = springAttr.getNamedItem("amplitude");
+		}
+	}
 }
