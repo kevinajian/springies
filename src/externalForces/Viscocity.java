b9cc6e9b362c79a955fcb32d9837bbca379e0e79
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-09-15 22:01:36 -0400

    started viscocity

---
+++ src/externalForces/Viscocity.java
@@ -0,0 +1,13 @@
+package externalForces;
+
+import jboxGlue.PhysicalObject;
+
+public class Viscocity extends Force {
+
+	@Override
+	public void applyForceToObject(PhysicalObject obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
