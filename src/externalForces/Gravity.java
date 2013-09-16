5d982b614839cdde3d66fb7b39361d8d2df1778d
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-09-15 22:48:07 -0400

    fixed gravity

--- src/externalForces/Gravity.java
+++ src/externalForces/Gravity.java
@@ -3,7 +3,16 @@ package externalForces;
 import org.jbox2d.common.Vec2;
 
 public class Gravity extends FixedForce {
+	
+	public Gravity(float degree, float magnitude){
+		this(convertDegreeToVector(degree).mul(magnitude));
+	}
+	
 	public Gravity(Vec2 force){
 		super(force);
+	}
+	
+	public static Vec2 convertDegreeToVector(float degree){
+		return new Vec2((float) Math.cos(degree)*-1, (float)Math.sin(degree)*-1);
 	}
 }
